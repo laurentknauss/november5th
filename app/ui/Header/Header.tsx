7bc11d0823abdd2aa998bfcbfba1de646388e7df
laurentknauss <laurentknauss@protonmail.com>
2025-03-17 12:17:54 +0100

    Fix CoinGecko logo rendering by creating custom SVG and using img tag

--- app/ui/Header/Header.tsx
+++ app/ui/Header/Header.tsx
@@ -23,7 +23,7 @@ const Header = ({
     <header className="w-full flex flex-col items-center pt-12 pb-10 relative">
       <div className="absolute left-6 top-6">
         <Link href="https://www.coingecko.com?utm_source=november5th&utm_medium=referral" target="_blank" rel="noopener noreferrer" className="flex items-center text-xs text-gray-500 hover:text-gray-700">
-          <Image src="/images/coingecko-logo.svg" alt="CoinGecko Logo" width={24} height={24} className="mr-2" />
+          <img src="/images/coingecko-logo.svg" alt="CoinGecko Logo" width={24} height={24} className="mr-2" />
           <span>Powered by CoinGecko</span>
         </Link>
       </div>
