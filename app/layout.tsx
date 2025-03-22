e83ff878d0c6557d67554d35fc3f560b014cb9f0
laurentknauss <laurentknauss@protonmail.com>
2025-03-22 15:43:48 +0100

    enhanced UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { config } from '@/app/config';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics, type BeforeSendEvent} from '@vercel/analytics/react'; 
 import AuroraBackground from '@/app/ui/AuroraBackground/AuroraBackground';
 import '@rainbow-me/rainbowkit/styles.css';
 import './globals.css';
+import { useEffect , useState } from 'react';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -17,6 +18,46 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
+
+
+  const [clientIp, setClientIp] = useState<string | null>(null);
+  
+  useEffect(() => {
+    // Fetch the client's IP address
+    const getClientIp = async () => {
+      try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        setClientIp(data.ip);
+      } catch (error) {
+        console.error('Error fetching IP:', error);
+      }
+    };
+    
+    getClientIp();
+  }, []);
+
+  const handleBeforeSend = (event: BeforeSendEvent): BeforeSendEvent | null => {
+    // Get excluded IPs from environment variable
+    const excludedIps = process.env.NEXT_PUBLIC_EXCLUDED_IPS?.split(',') || [];
+    
+    // Don't track if client IP is in the excluded list
+    if (clientIp && excludedIps.includes(clientIp)) {
+      console.log('Analytics tracking disabled for this IP');
+      return null;
+    }
+    
+    return event;
+  };
+
+
+
+
+
+
+
+
   return (
     <html lang="en" className="h-full">
       <body className="h-full">
