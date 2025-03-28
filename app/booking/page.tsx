f664754de1bfb11a41818577991296eb9d333a63
laurentknauss <laurentknauss@protonmail.com>
2025-03-28 12:26:38 +0100

    modified metadata.js and layout.tsx

--- app/booking/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import Booking from "../ui/Booking/Booking";
-
-export default function BookingPage() {
-  return <Booking />;
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
+/*
+* The metadata is dynamically imported and used in RootLayout.tsx. 
+* All necessary OpenGraph andTwitter tags are included for better SEO and 
+* social media sharing 
+*/
+
+
 'use client';
 
+import Head from 'next/head';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { config } from '@/app/config';
 import AuroraBackground from '@/app/ui/AuroraBackground/AuroraBackground';
+import { metadata} from './metadata';
 import '@rainbow-me/rainbowkit/styles.css';
 import './globals.css';
 
@@ -18,6 +27,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full overflow-x-hidden">
+       <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:site" content="@november5th" />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.images[0]} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+      </Head>
+
       <body className="h-full">
         <WagmiProvider config={config}>
           <QueryClientProvider client={queryClient}>
