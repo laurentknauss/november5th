1407559798e7be40f809822d95c025d06e6821a0
laurentknauss <laurentknauss@protonmail.com>
2025-04-05 17:20:36 +0200

    added seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,48 @@
 * The metadata is dynamically imported and used in RootLayout.tsx. 
 * All necessary OpenGraph andTwitter tags are included for better SEO and 
 * social media sharing 
+* Layout.tsx is a pure Server component handling the overall structure of the app 
+* namely the metadata, global styles, and the Providers component.
 */
 
 
-'use client';
 
-import Head from 'next/head';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { config } from '@/app/config';
-import AuroraBackground from '@/app/ui/AuroraBackground/AuroraBackground';
-import { metadata} from './metadata';
+import type  { Metadata } from 'next';
 import '@rainbow-me/rainbowkit/styles.css';
 import './globals.css';
+import Providers from '@/app/providers';
 
-// Create a client
-const queryClient = new QueryClient();
+
+export const metadata: Metadata = {
+  
+    title: "November 5th - Blockchain Voting",
+    description: "Secure blockchain-based voting platform",
+    metadataBase: new URL("https://www.november5th.xyz"), 
+    openGraph: {
+      title: "November 5th - Blockchain Voting",
+      description: "Secure blockchain-based voting platform",
+      
+      images: [{
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "November 5th Blockchain Voting Platform",
+      }],
+      type: "website",
+      siteName: "November 5th - Blockchain Voting",
+      locale: "en_US",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "November 5th - Blockchain Voting",
+      description: "Secure blockchain-based voting platform",
+      images: ["/opengraph-image.png"],
+    },
+  };
+
+
+
+
 
 export default function RootLayout({
   children,
@@ -26,35 +51,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full overflow-x-hidden">
-       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="twitter:card" content={metadata.twitter.card} />
-        <meta name="twitter:site" content="@november5th" />
-        <meta name="twitter:creator" content={metadata.twitter.creator} />
-        <meta name="twitter:title" content={metadata.twitter.title} />
-        <meta name="twitter:description" content={metadata.twitter.description} />
-        <meta name="twitter:image" content={metadata.twitter.images[0]} />
-        <meta property="og:type" content={metadata.openGraph.type} />
-        <meta property="og:locale" content={metadata.openGraph.locale} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:site_name" content={metadata.openGraph.siteName} />
-        <meta property="og:title" content={metadata.openGraph.title} />
-        <meta property="og:description" content={metadata.openGraph.description} />
-        <meta property="og:image" content={metadata.openGraph.images[0].url} />
-      </Head>
+    <html lang="en">
+      <head>
+
+      </head>
 
       <body className="h-full">
-        <WagmiProvider config={config}>
-          <QueryClientProvider client={queryClient}>
-            <RainbowKitProvider>
-              <AuroraBackground>
-                {children}
-              </AuroraBackground>
-            </RainbowKitProvider>
-          </QueryClientProvider>
-        </WagmiProvider>
+        <Providers>
+           { children } 
+        </Providers>
       </body>
     </html>
   );
